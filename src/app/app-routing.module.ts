27e4df21bb8d1301c93deb603f8b12aaf356258a
scourtin <scourtin@excilys.com>
2018-06-20 14:24:20 +0200

    [FIX] résolution sur les routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,9 @@ import { CompanyEditComponent } from './company/company-edit/company-edit.compon
 import { CompanyComponent } from './company/company/company.component';
 import { CompaniesComponent } from './company/companies/companies.component';
 import {ComputerAddComponent} from './computer/computer-add/computer-add.component';
-import {ComputerEditComponent} from './computer/computer-edit/computer-edit.component';
 import {ComputerComponent} from './computer/computer/computer.component';
 import {ComputersComponent} from './computer/computers/computers.component';
+import {ComputerEditComponent} from './computer/computer-edit/computer-edit.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -20,11 +20,17 @@ const routes: Routes = [
   { path: 'company/edit/:id', component: CompanyEditComponent},
   { path: 'company/:id', component: CompanyComponent},
   { path: 'company/search/:search', component: CompaniesComponent},
+  { path: 'company/search/:search/page/:page', component: CompaniesComponent},
+  { path: 'company/search/:search/page/:page/limit/:limit', component: CompaniesComponent},
   { path: 'company', component: CompaniesComponent},
+  { path: 'company/page/:page', component: CompaniesComponent},
+  { path: 'company/page/:page/limit/:limit', component: CompaniesComponent},
   { path: 'computer/add', component: ComputerAddComponent},
   { path: 'computer/edit/:id', component: ComputerEditComponent},
   { path: 'computer/:id', component: ComputerComponent},
   { path: 'computer/search/:search', component: ComputersComponent},
+  { path: 'computer/search/:search/page/:page', component: ComputersComponent},
+  { path: 'computer/search/:search/page/:page/limit/:limit', component: ComputersComponent},
   { path: 'computer', component: ComputersComponent},
   { path: '**', redirectTo: 'login' },
 ];
